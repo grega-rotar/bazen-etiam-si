5b45705f07e93230d550339b77e8d2870c7cab81
Grega Rotar <grega@etiam.si>
2025-06-29 16:51:25 +0200

    view when update

--- update.sh
+++ update.sh
@@ -2,3 +2,5 @@ git pull origin main                                                            
 sudo docker build . -t registry.etiam.si/public/bazen-lights
 sudo docker push registry.etiam.si/public/bazen-lights
 kubectl rollout restart deployment bazen-lights -n pirnice-hq
+
+kubectl get all -n pirnice-hq
