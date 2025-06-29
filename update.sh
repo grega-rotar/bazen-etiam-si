9226b94810fc01ae0e2c515f366f3ac59aa758d9
Grega Rotar <grega@etiam.si>
2025-06-29 16:48:43 +0200

    new icons & update script

---
+++ update.sh
@@ -0,0 +1,4 @@
+git pull origin main                                                                                                                                                        sudo docker build . -t registry.etiam.si/public/bazen-lights                                                                                                                sudo docker push registry.etiam.si/public/bazen-lights                                                                                                                      kubectl rollout restart deployment bazen-lights -n pirnice-hq                                                                                                                                                                                git pull origin main
+sudo docker build . -t registry.etiam.si/public/bazen-lights
+sudo docker push registry.etiam.si/public/bazen-lights
+kubectl rollout restart deployment bazen-lights -n pirnice-hq
